6a52094d7268b77d8e66e9be4f0478b090548a60
Rodolfo Perez <rodolfobeste@gmail.com>
2018-11-11 23:07:29 +0200

    Added functionfor random background color and added tag property to quote objects

--- js/script.js
+++ js/script.js
@@ -4,39 +4,47 @@
 const quotes = [
   {
     quote: "Try again. Fail again. Fail better.",
-    source: "Samuel Beckett"
+    source: "Samuel Beckett",
+    tags: "Existentialism"
   },
   {
     quote: "There is scarcely any passion without struggle.",
     source: "Albert Camus",
-    citation: "The Myth of Sisyphus and Other Essays"
+    citation: "The Myth of Sisyphus and Other Essays",
+    tags: "Existentialism"
   },
   {
     quote: "One repays a teacher badly if one always remains nothing but a pupil.",
     source: "Friedrich Nietzsche",
-    citation: "Thus Spoke Zarathustra"
+    citation: "Thus Spoke Zarathustra",
+    tags: "Existentialism"
   },
   {
     quote: "The world is, of course, nothing but our conception of it.",
-    source: "Anton Chekhov"
+    source: "Anton Chekhov",
+    tags: "Existentialism"
   },
   {
     quote: "Man is nothing else but what he makes of himself.",
     source: "Jean-Paul Sartre",
-    citation: "Existentialism and Human Emotions"
+    citation: "Existentialism and Human Emotions",
+    tags: "Existentialism"
   },
   {
     quote: "if we possess a why of life we can put up with almost any how.",
     source: "Friedrich Nietzsche",
-    citation: "Twilight of the Idols"
+    citation: "Twilight of the Idols",
+    tags: "Existentialism"
   },
   {
     quote: "Your positive action combined with positive thinking results in success",
-    source: "Shiv Khera"
+    source: "Shiv Khera",
+    tags: "Positive mind"
   },
   {
     quote: "Adopting the right attitude can convert a negative stress into a positive one",
-    source: "Hans Selye"
+    source: "Hans Selye",
+    tags: "Positive mind"
   },
 ];
 
@@ -46,6 +54,17 @@ const quotes = [
 function getRandomQuote(array) {
   return array[Math.floor(Math.random() * Math.floor(8))];
 }
+
+// function for creating a random background color
+
+function randomBC () {
+
+  let rgb = 'rgb(' + Math.floor(Math.random() * Math.floor(255)) + ',' + Math.floor(Math.random() * Math.floor(255)) + ',' + Math.floor(Math.random() * Math.floor(255)) + ')';
+
+  return rgb
+}
+
+
 
 
 // function to output the selected random quote
@@ -64,6 +83,7 @@ function printQuote() {
  }
 
  document.querySelector("#quote-box").innerHTML =  string;
+ document.querySelector("body").style.backgroundColor = randomBC();
 
 }
 
